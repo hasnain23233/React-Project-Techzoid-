d797e0dcc3244e4d719d9a6c4822194c36c30225
Hasnain Khushi <hasnainkhushii@gmail.com>
2026-01-30 13:41:23 +0500

    In this class we learn about useState and how to use it in React

--- src/components/upperNavbar.jsx
+++ src/components/upperNavbar.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
-import Logo from './../../public/logo.jpeg'
+import logo from './../../public/logo.jpeg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee, faHome } from '@fortawesome/free-solid-svg-icons';
+import { faGithub , faFacebook } from '@fortawesome/free-brands-svg-icons';
+
 
 const upperNavbar = () => {
     return (
         <div>
-            <ul className="flex gap-5 justify-center h-16 bg-gray-400 items-center text-white">
-                <img src={Logo} alt="" className='w-14 rounded-full' />
+            {/* <img src={logo} alt="" / */}
+            <h1 className='lg:hidden block'>Menu</h1>
+            <ul className="lg:flex  h-20  items-center text-xl font-semibold font-mono text-red-800 lg:flex-row flex-col hidden ">
                 <li>Home</li>
                 <li>About</li>
                 <li>Services</li>
                 <li>Contact Us</li>
             </ul>
+
+             <h1>Welcome! <FontAwesomeIcon icon={faFacebook} /></h1>
         </div>
     )
 }
--- src/home.jsx
+++ src/home.jsx
@@ -1,17 +1,37 @@
-import React from 'react'
+import React , {useState} from 'react'
 
 const home = () => {
-    const name = 'WellCome to our React App'
-    const number = 4 + 5
+
+    
+    const [state, setState] = useState(0)
+    const [user , setUser] = useState(false)
+
+    let handlePlusFun = ()=>{
+        setState(state + 1)
+        if(state === 10){
+            setUser(true)
+        }
+    }
+      let handleSubractFun = ()=>{
+        setState(state - 1)
+    }
+ 
+ 
+
 
     return (
-        <div>
-            <div className="h-screen bg-black flex items-center justify-center  text-white">
-                <div>
-                    <h1 className='text-4xl font-bold'>{name}</h1>
-                    <p>{number}</p>
+        <div className='h-screen bg-black  text-white'>
+            <div className=" flex items-center justify-center  text-white">
+
+                <div className='flex items-center gap-8'>   
+                    <button className='bg-white p-1 text-lg font-bold w-8 h-8 rounded-full text-black' onClick={handlePlusFun}>+</button>
+                    <h1 className='text-5xl font-bold'>{state}</h1>
+                    <button className='bg-white p-1 text-lg font-bold w-8 h-8 rounded-full text-black' onClick={handleSubractFun}>-</button>
                 </div>
+
             </div>
+            
+                {user ? <h1>Wellcome to our React app </h1> : <h1>Sorry you can`t login</h1>}
         </div>
     )
 }
